8eea2c3387e5f632d28e5157c0cc265917275d29
humphreynganga <humphreynganga@gmail.com>
2016-11-27 23:37:48 +0300

    modified main activity
    added  new file "testaddfile"

--- app/src/main/java/com/example/user/pushup/MainActivity.java
+++ app/src/main/java/com/example/user/pushup/MainActivity.java
@@ -13,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void testUpload(){
         int i= 1+1;
+
+        int ii= 2+2;
+
     }
 }
 
---
+++ app/src/main/java/com/example/user/pushup/testaddfile.java
@@ -0,0 +1,8 @@
+package com.example.user.pushup;
+
+/**
+ * Created by user on 27-Nov-16.
+ */
+
+public class testaddfile {
+}
